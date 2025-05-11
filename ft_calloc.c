178f6092cf32dd4cacf1ec41d7ca3e5ae329e1ce
HarukaOmura <harucha.cha.cha0723@gmail.com>
2025-05-11 19:59:18 +0900

    if (count == 0 || size == 0)のときNULLを返すようにした

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: harukaomura <harukaomura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 20:49:48 by vscode            #+#    #+#             */
-/*   Updated: 2025/05/11 16:30:49 by harukaomura      ###   ########.fr       */
+/*   Updated: 2025/05/11 19:41:23 by harukaomura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -18,7 +19,7 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	total_size;
 
 	if (count == 0 || size == 0)
-		return (malloc(0));
+		return (NULL);
 	if (size > SIZE_MAX / count)
 		return (NULL);
 	total_size = count * size;
